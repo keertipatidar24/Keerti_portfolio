7d8a7066ab995d11a40ce8fe0cae5c048cf4f697
keertipatidar24 <keertipatidar24@navgurukul.org>
2025-02-04 18:13:35 +0530

    Your commit message for the submodule

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "backend development", "web designing", "android development", "Full Stack Web Development"],
+    strings: ["frontend development", "web designing", ],
     loop: true,
     typeSpeed: 60,
     backSpeed: 30,
